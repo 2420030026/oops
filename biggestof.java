5a91dce3ace5a30549f5cef52fb1ab5736348fbb
abhilash <2420030026@klh.edu.in>
2025-08-12 14:03:23 +0530

    program10

---
+++ biggestof.java
@@ -0,0 +1,13 @@
+package project;
+
+public class biggestof {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=41;
+		int y=45;
+		System.out.println((x>y)?x:y);
+
+	}
+
+}
